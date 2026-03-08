036f6532d912f5799cbc75fc38835503a3842843
Ryan <ryansaftje123@gmail.com>
2026-03-08 22:23:36 +0800

    Initial Commit

---
+++ InvestiGoldIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InvestiGoldIOS
+//
+//  Created by Ryan Tjendana on 08/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InvestiGoldIOS/InvestiGoldIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvestiGoldIOSApp.swift
+//  InvestiGoldIOS
+//
+//  Created by Ryan Tjendana on 08/03/26.
+//
+
+import SwiftUI
+
+@main
+struct InvestiGoldIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
